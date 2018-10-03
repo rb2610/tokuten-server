d4ef151bb8581da1b5bcdc8e311b6a1e351c622f
rb2610 <rb2610@hotmail.co.uk>
2018-10-03 22:47:24 +0100

    Working server tests

--- src/server.js
+++ src/server.js
@@ -3,8 +3,27 @@ import express from "express";
 const app = express();
 const port = process.env.port || 3001;
 
-app.get('/api/hello', (request, response) => {
-    response.send({ 'message': 'MANGOES (は美味しいよ)' });
+var data = {
+    "data": {
+      "user": {
+        "name": "Foo",
+        "wins": 5
+      },
+      "user": {
+        "name": "Roo",
+        "wins": 2
+      }
+    }
+  };
+
+app.get("/api/status", (request, response) => {
+  response.json({ status: "Most Excellent" });
+});
+
+app.get("/api/scoreData", (request, response) => {
+  response.json(data);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+export default app;
--- test/server.test.js
+++ test/server.test.js
@@ -1,11 +1,50 @@
-import server from "server.js";
-import request from "supertest";
-import expect from "chai";
+import server from "../src/server";
+import chai from "chai";
+import chaiHttp from "chai-http";
 
-describe("Server", () => {
-    describe("scoreData", () => {
-        it("should return a JSON containing score data", () => {
+chai.use(chaiHttp);
+const request = chai.request;
+const expect = chai.expect;
 
+describe("Server API", () => {
+  describe("scoreData", () => {
+    it("should return a JSON containing score data", () => {
+      return request(server)
+        .get("/api/scoreData")
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body).to.deep.equal({
+            "data": {
+              "user": {
+                "name": "Foo",
+                "wins": 5
+              },
+              "user": {
+                "name": "Roo",
+                "wins": 2
+              }
+            }
+          });
         })
-    })
-})
+        .catch(error => {
+          throw error;
+        });
+    });
+  });
+
+  describe("status", () => {
+    it("should return a status JSON", () => {
+      return request(server)
+        .get("/api/status")
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body).to.deep.equal({
+            status: "Most Excellent"
+          });
+        })
+        .catch(error => {
+          throw error;
+        });
+    });
+  })
+});
