adc0887aae0860281dbfdfcba341d902352895be
rb2610 <rb2610@hotmail.co.uk>
2018-11-10 16:29:53 +0000

    Added CORS support.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
+import cors from "cors";
 import express from "express";
 import ScoresRepository from "./scoresRepository";
 
 const app = express();
 const port = process.env.PORT || 3001;
+const clientUrl = process.env.CLIENT_URL || "http://localhost:3000";
 
 const scoresRepository : ScoresRepository = new ScoresRepository();
+
+const corsOptions = {
+  origin: clientUrl
+}
 
 app.get("/api/status", (request, response) => {
   response.json({ status: "Most Excellent" });
 });
 
-app.get("/api/scoreData", (request, response) => {
+app.get("/api/scoreData", cors(corsOptions), (request, response) => {
   scoresRepository
     .scores()
     .then(scores => {
